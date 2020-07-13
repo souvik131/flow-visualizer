faa76be8bd2bbfade69dbc770c08889d3eefb84a
Souvik <souvik131@gmail.com>
2020-07-14 02:15:04 +0530

    msg

--- src/services/FlowFormatter.js
+++ src/services/FlowFormatter.js
@@ -96,7 +96,6 @@ class FlowFormatter{
 			let processor = getFlowProcessor(startPoint,clubbedNodes)
 			this.packagedOutput=processor.start().dataSet
 		})
-		console.log(this.packagedOutput)
 		this.restructureData()
 		return this
 	}
